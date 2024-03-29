8504e08a9c92490258d1d9117f97ffe61a357c2f
FavianRP <favfianrafi@gmail.com>
2024-03-29 12:56:06 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Toggle class active hamburger menu
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger di klik
+document.querySelector("#hamburger-menu").onclick = (e) => {
+  navbarNav.classList.toggle("active");
+  e.preventDefault();
+};
+
+// klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+// Klik di luar elemen
+const hm = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
