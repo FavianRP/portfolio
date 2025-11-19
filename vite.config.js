fb8e5801e72ea86cae84286482f219a607272274
FavianRP <favfianrafi@gmail.com>
2025-11-19 10:49:12 +0700

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio/',   // ← WAJIB sesuai nama repo kamu!
   plugins: [react()],
 })
+
